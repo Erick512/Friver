6f1c0ef15c81f176156ee4a7c01b1c7ee9aa8289
Erick Alonso <alonsoerick@icloud.com>
2022-09-15 02:24:52 -0700

    added port

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use((req, res, next) => {
       "<h1>Page not found on the server</h1>")
 })
  
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 8000, ()=>{
     console.log('Server is running, you better catch it!')
 })    
